8eddd52b2e21c2b0efbaaa87c15064c2fad075fd
devMakiJs <gaelautomat.com>
2026-03-08 22:02:49 -0600

    chore: ensure vercel adapter is active

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'; // Importamos el nuevo adaptador
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server", // Mantenemos el modo servidor para SSR
-  adapter: vercel()  // Cambiamos el adaptador a Vercel
+  output: 'server',
+  adapter: vercel(),
 });
